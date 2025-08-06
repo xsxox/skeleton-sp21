831e8e0ee0e8096a330b28b43d57618f5a492c47
your name <your email>
2025-08-06 15:05:02 +0800

    GuitarHeroLite

--- proj1/gh2/GuitarString.java
+++ proj1/gh2/GuitarString.java
@@ -1,55 +1,54 @@
 package gh2;
 
-// TODO: uncomment the following import once you're ready to start this portion
-// import deque.Deque;
-// TODO: maybe more imports
+import deque.Deque;
+import deque.LinkedListDeque;
 
-//Note: This file will not compile until you complete the Deque implementations
 public class GuitarString {
-    /** Constants. Do not change. In case you're curious, the keyword final
-     * means the values cannot be changed at runtime. We'll discuss this and
-     * other topics in lecture on Friday. */
-    private static final int SR = 44100;      // Sampling Rate
-    private static final double DECAY = .996; // energy decay factor
+
+    private static final int SR = 44100;      // 采样率
+    private static final double DECAY = .996; // 能量衰减因子
 
     /* Buffer for storing sound data. */
-    // TODO: uncomment the following line once you're ready to start this portion
-    // private Deque<Double> buffer;
+     private Deque<Double> buffer;
 
-    /* Create a guitar string of the given frequency.  */
+    /* 用给定的 frequency 创造一个吉他弦 */
     public GuitarString(double frequency) {
-        // TODO: Create a buffer with capacity = SR / frequency. You'll need to
-        //       cast the result of this division operation into an int. For
-        //       better accuracy, use the Math.round() function before casting.
-        //       Your should initially fill your buffer array with zeros.
+        //  创建一个缓冲区（buffer），其容量（capacity）为 采样率（SR）除以频率（frequency）。
+        //  这个除法运算的结果强制转换为整数（int）。为了获得更高的精度，在强制转换之前使用 Math.round () 函数。
+        //  初始时，你的缓冲区数组应填充零。
+
+        int capacity = (int) Math.round( SR / frequency);
+        buffer = new LinkedListDeque<>();
+        for(int i=0;i<capacity;i++){
+            buffer.addLast(0.0);
+        }
     }
 
 
-    /* Pluck the guitar string by replacing the buffer with white noise. */
+    /* 通过用白噪声替换缓冲区来模拟拨动吉他弦的动作。 */
     public void pluck() {
-        // TODO: Dequeue everything in buffer, and replace with random numbers
-        //       between -0.5 and 0.5. You can get such a number by using:
-        //       double r = Math.random() - 0.5;
+        // 清空缓冲区中的所有内容，并用-0.5到0.5之间的随机数重新填充
+        // 可以通过以下方式获取这样的随机数：
+        // double r = Math.random() - 0.5;
         //
-        //       Make sure that your random numbers are different from each
-        //       other. This does not mean that you need to check that the numbers
-        //       are different from each other. It means you should repeatedly call
-        //       Math.random() - 0.5 to generate new random numbers for each array index.
+        // 确保生成的随机数彼此不同。这并不是说你需要检查每个数字是否与其他数字不同，
+        // 而是意味着你应该为每个数组索引重复调用Math.random() - 0.5来生成新的随机数。
+        for(int i = 0; i<buffer.size(); i++){
+            double r = Math.random() - 0.5;
+            buffer.removeFirst();
+            buffer.addLast(r);
+        }
     }
 
-    /* Advance the simulation one time step by performing one iteration of
-     * the Karplus-Strong algorithm.
-     */
+    /* 执行一次 the Karplus-Strong algorithm的迭代*/
+
     public void tic() {
-        // TODO: Dequeue the front sample and enqueue a new sample that is
-        //       the average of the two multiplied by the DECAY factor.
-        //       **Do not call StdAudio.play().**
+        double newDouble = (buffer.removeFirst() + buffer.get(0)) * 0.5 * DECAY;
+        buffer.addLast(newDouble);
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
-        // TODO: Return the correct thing.
-        return 0;
+        return buffer.get(0);
     }
 }
-    // TODO: Remove all comments that say TODO when you're done.
